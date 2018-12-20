ed45150ebd350730229a4a06720d6d75df1558f9
programador <programador@BOAD9751105>
2018-12-20 12:51:02 -0500

    Omitiendo propiedades en el config

--- src/main/java/com/bdg/dashboard/MongoConfig.java
+++ src/main/java/com/bdg/dashboard/MongoConfig.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -16,7 +15,6 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
@@ -34,26 +32,28 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
 	private final Logger log = LoggerFactory.getLogger(MongoConfig.class);
 
-	@Value("${spring.data.mongodb.host}")
+	/*@Value("${spring.data.mongodb.host}")
 	private String host;
 	@Value("${spring.data.mongodb.port}")
 	private Integer port;
 	@Value("${spring.data.mongodb.username}")
 	private String username;
-	@Value("${spring.data.mongodb.database}")
-	private String database;
+	
 	@Value("${spring.data.mongodb.password}")
-	private String password;
+	private String password;*/
 	@Value("${spring.data.mongodb.uri}")
 	private String mongoURI;
 
+	@Value("${spring.data.mongodb.database}")
+	private String database;
+	
 	@Autowired
 	private Environment env;
 
 	@Override
 	public MongoClient mongoClient() {
-		log.info("Conectando a MongoDb host:" + host + ", username:" + username + ", database:" + database
-				+ ", password:" + password);
+		//log.info("Conectando a MongoDb host:" + host + ", username:" + username + ", database:" + database
+			//	+ ", password:" + password);
 		log.info("Conectando a Mongo URI host:" + mongoURI);
 
 		// return new MongoClient(Collections.singletonList(new ServerAddress(host,

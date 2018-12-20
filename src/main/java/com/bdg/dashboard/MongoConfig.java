e7f45d62e97949024024b9c4b055ea93650fea26
programador <programador@BOAD9751105>
2018-12-20 09:58:11 -0500

    Ajuste de conexión mongo db

--- src/main/java/com/bdg/dashboard/MongoConfig.java
+++ src/main/java/com/bdg/dashboard/MongoConfig.java
@@ -3,7 +3,12 @@
  */
 package com.bdg.dashboard;
 
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,6 +17,8 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 /**
  * @author programador
@@ -25,14 +32,29 @@ import com.mongodb.MongoClient;
 //@EnableWebMvc
 public class MongoConfig extends AbstractMongoConfiguration {
 
+	private final Logger log = LoggerFactory.getLogger(MongoConfig.class);
+	@Value("${spring.data.mongodb.host}")
+	private String host;
+	@Value("${spring.data.mongodb.port}")
+	private Integer port;
+	@Value("${spring.data.mongodb.username}")
+	private String username;
+	@Value("${spring.data.mongodb.database}")
+	private String database;
+	@Value("${spring.data.mongodb.password}")
+	private String password;
+
 	@Autowired
 	private Environment env;
-	
+
 	@Override
 	public MongoClient mongoClient() {
-		return new MongoClient(env.getProperty("spring.data.mongodb.host"), 
-				Integer.parseInt(env.getProperty("spring.data.mongodb.port")));
+		return new MongoClient(Collections.singletonList(new ServerAddress(host, port)),
+				Collections.singletonList(MongoCredential.createCredential(username, database, password.toCharArray())));
+
+		//return new MongoClient(host, port);
 	}
+
 
 	@Override
 	protected String getDatabaseName() {
